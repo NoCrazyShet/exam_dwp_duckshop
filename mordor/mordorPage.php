44b23f11f5a98838781a8088628e466f8d2e6eff
NoCrazyShet <j.pettit51@gmail.com>
2018-04-17 10:46:57 +0200

    Page redirects included

--- mordor/mordorPage.php
+++ mordor/mordorPage.php
@@ -15,8 +15,8 @@
                 <div class="nav-wrapper">
                     <a href="#" class="brand-logo">Logo</a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="#">Company Information</a></li>
-                        <li><a href="#">Products</a></li>
+                        <li><a href="mordorPage.php?page=compInfo">Company Information</a></li>
+                        <li><a href="mordorPage.php?page=products">Products</a></li>
                     </ul>
                 </div>
             </div>
@@ -24,10 +24,20 @@
     </header>
     <div class="container">
         <h1 align="center">Welcome to the backend</h1>
+            <?php
+            $allowed = array('compInfo', 'products');
+            $page = ( isset($_GET['page']) ) ? $_GET['page'] : 'mordorPage';
+            if ( in_array($page, $allowed) ) {
+            include("./$page.php");
+            }
+            ?>
+
+
+
+
             <form action="includes/logoutController.php" method="get">
                 <input type="submit" value="Click here to log out!" class="waves-effect waves-light btn-large">
             </form>
-
     </div>
     </body>
     </html>
