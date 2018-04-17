ca318877b44aed9d6af2cdaa6452ff9716c8a72b
NoCrazyShet <j.pettit51@gmail.com>
2018-04-17 10:32:36 +0200

    Containers

--- mordor/mordorPage.php
+++ mordor/mordorPage.php
@@ -11,21 +11,23 @@
     <body>
     <header>
         <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo">Logo</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="#">Company Information</a></li>
-                    <li><a href="#">Products</a></li>
-                </ul>
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="#">Company Information</a></li>
+                        <li><a href="#">Products</a></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
-    <h1 align="center">Welcome to the backend</h1>
+    <div class="container">
+        <h1 align="center">Welcome to the backend</h1>
+            <form action="includes/logoutController.php" method="get">
+                <input type="submit" value="Click here to log out!" class="waves-effect waves-light btn-large">
+            </form>
 
-        <form action="includes/logoutController.php" method="get">
-            <input type="submit" value="Click here to log out!" class="waves-effect waves-light btn-large">
-        </form>
-
-
+    </div>
     </body>
     </html>
