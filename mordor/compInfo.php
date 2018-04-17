2751a9d378aefaad1e433e621a46d5957ecca2ab
NoCrazyShet <j.pettit51@gmail.com>
2018-04-17 12:18:00 +0200

    Input fields updated with SQL SELECT, functionality to update still not included

--- mordor/compInfo.php
+++ mordor/compInfo.php
@@ -1,13 +1,43 @@
-<?php ?>
+<?php
+require_once("includes/connection.php");
+
+$stmt = $connection->query("SELECT CVR, logo, logoText, aboutUs, zipCode, street, streetNumber FROM companyinfo");
+$compInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+var_dump($compInfo);
+?>
 
 <div class="row">
     <form class="col s12">
       <div class="row">
-        <div class="input-field col s12">
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-          <label for="textarea1">Textarea</label>
+        <div class="input-field col s12 m6">
+              <textarea id="CVR" class="materialize-textarea"><?php echo $compInfo['CVR'];?></textarea>
+              <label for="CVR">CVR</label>
+        </div>
+        <div class="input-field col s12 m6">
+              <textarea id="logoText" class="materialize-textarea"><?php echo $compInfo['logoText'];?></textarea>
+              <label for="logoText">Logo Text</label>
         </div>
       </div>
+        <div class="row">
+            <div class="input-field col s12 m12">
+              <textarea id="aboutUs" class="materialize-textarea"><?php echo $compInfo['aboutUs'];?></textarea>
+              <label for="aboutUs">About Us</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12 m6">
+              <textarea id="street" class="materialize-textarea"><?php echo $compInfo['street'];?></textarea>
+              <label for="street">Street</label>
+            </div>
+            <div class="input-field col s6 m3">
+                <textarea id="streetNumber" class="materialize-textarea"><?php echo $compInfo['street'];?></textarea>
+                <label for="streetNumber">Street number</label>
+            </div>
+            <div class="input-field col s6 m3">
+                <textarea id="zipCode" class="materialize-textarea"><?php echo $compInfo['zipCode'];?></textarea>
+                <label for="zipCode">Zip Code</label>
+            </div>
+        </div>
     </form>
   </div>
 
--- mordor/mordorPage.php
+++ mordor/mordorPage.php
@@ -7,6 +7,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15"/>
         <link rel="stylesheet" type="text/css" href="../main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
     <header>
