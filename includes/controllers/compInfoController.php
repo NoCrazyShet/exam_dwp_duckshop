ee36da860fbeb46d43804b7a6290a1dc8b99d54d
NoCrazyShet <j.pettit51@gmail.com>
2018-04-18 14:33:03 +0200

    Update company info working

--- includes/controllers/compInfoController.php
+++ includes/controllers/compInfoController.php
@@ -1,9 +1,28 @@
 <?php
-require_once("../includes/permanents/connection.php");
-
-    $stmt = $connection->query("SELECT * FROM companyinfo");
+require_once(__DIR__ . "/../permanents/connection.php");
+$query = "SELECT * FROM companyinfo";
+    $stmt = $connection->query($query);
     $compInfo = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    function updateCompInfo() {
+    if(isset($_GET['action'])){
+        $action = $_GET["action"];
+            if ($action == "update")
+            {
+                $logoText = $_POST["logoText"];
+                $aboutUs = $_POST['aboutUs'];
+                $street = $_POST['street'];
+                $streetNr = $_POST['streetNumber'];
+                $zip = $_POST['zipCode'];
+                $CVR = $compInfo['CVR'];
+                $query2 = "UPDATE companyinfo SET logoText = '{$logoText}', aboutUs = '{$aboutUs}', street = '{$street}', streetNumber = '{$streetNr}', zipCode = '{$zip}' WHERE CVR= '{$CVR}'";
 
+                $stmt = $connection->prepare($query2);
+                $stmt->execute();
+
+                include("../permanents/functions.php");
+                redirect_to("../../mordor/mordorPage.php?page=compInfo");
+            }
     }
+
+
+
--- mordor/compInfo.php
+++ mordor/compInfo.php
@@ -3,34 +3,30 @@ require_once("../includes/controllers/compInfoController.php");
 ?>
 
 <div class="row">
-    <form class="col s12">
+    <form class="col s12" name="lars" id="lars" method="POST" action="../includes/controllers/compInfoController.php?action=update">
       <div class="row">
-        <div class="input-field col s12 m6">
-              <textarea id="CVR" class="materialize-textarea"><?php echo $compInfo['CVR'];?></textarea>
-              <label for="CVR">CVR</label>
-        </div>
-        <div class="input-field col s12 m6">
-              <textarea id="logoText" class="materialize-textarea"><?php echo $compInfo['logoText'];?></textarea>
+        <div class="input-field col s12 m12">
+              <textarea id="logoText" name="logoText" class="materialize-textarea"><?php echo $compInfo['logoText'];?></textarea>
               <label for="logoText">Logo Text</label>
         </div>
       </div>
         <div class="row">
             <div class="input-field col s12 m12">
-              <textarea id="aboutUs" class="materialize-textarea"><?php echo $compInfo['aboutUs'];?></textarea>
+              <textarea id="aboutUs" name="aboutUs" class="materialize-textarea"><?php echo $compInfo['aboutUs'];?></textarea>
               <label for="aboutUs">About Us</label>
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12 m6">
-              <textarea id="street" class="materialize-textarea"><?php echo $compInfo['street'];?></textarea>
+              <textarea id="street" name="street" class="materialize-textarea"><?php echo $compInfo['street'];?></textarea>
               <label for="street">Street</label>
             </div>
             <div class="input-field col s6 m3">
-                <textarea id="streetNumber" class="materialize-textarea"><?php echo $compInfo['streetNumber'];?></textarea>
+                <textarea id="streetNumber" name="streetNumber" class="materialize-textarea"><?php echo $compInfo['streetNumber'];?></textarea>
                 <label for="streetNumber">Street number</label>
             </div>
             <div class="input-field col s6 m3">
-                <textarea id="zipCode" class="materialize-textarea"><?php echo $compInfo['zipCode'];?></textarea>
+                <textarea id="zipCode" name="zipCode" class="materialize-textarea"><?php echo $compInfo['zipCode'];?></textarea>
                 <label for="zipCode">Zip Code</label>
             </div>
         </div>
