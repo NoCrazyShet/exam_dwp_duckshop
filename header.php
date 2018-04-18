3f131f89d2846bc7a28949fdb75ff788d347e3b5
Zeljko laptop <duranovic46@gmail.com>
2018-04-18 09:22:10 +0200

    added footer and header include files

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php ?>
+        <nav>
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="#">Something1</a></li>
+                        <li><a href="#">Something2</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <?php
+?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15"/>
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+    <body>
+        <header>
+            <?php
+            include ("header.php");
+            ?>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer class="page-footer">
+            <?php
+            include ("footer.php");
+            ?>
+        </footer>
+    </body>
+</html>
